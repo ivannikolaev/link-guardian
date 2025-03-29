46f53dee27f5daa81daf9482bd0b404dd44c83fb
i.a.nikolaev <i.a.nikolaev@tbank.ru>
2025-03-29 17:20:43 +0500

    Minimal Spring Boot project

---
+++ src/main/java/com/github/ivannikolaev/link_guardian/LinkGuardianApplication.java
@@ -0,0 +1,13 @@
+package com.github.ivannikolaev.link_guardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkGuardianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ivannikolaev/link_guardian/LinkGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ivannikolaev.link_guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
